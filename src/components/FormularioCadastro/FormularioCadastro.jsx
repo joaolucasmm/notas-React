74ea911b789d40aa0c368018cfa5dfabc27e8423
João Lucas <joaol_mm@hotmail.com>
2022-03-31 17:55:47 -0300

    Adicionando exclusão de notas e separando dados em observables

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,8 +7,21 @@ class FormularioCadastro extends Component {
     this.titulo = "";
     this.texto = "";
     this.categoria = "Sem categoria";
+    this.state = {categorias:[]}
+    this._novasCategorias = this._novasCategorias.bind(this)
   }
-  
+
+  componentDidMount(){
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount(){
+    this.props.categorias.desinscrever(this._novasCategorias);
+  }  
+  _novasCategorias(categorias){
+    this.setState({...this.state,categorias})
+  }
+
   _handleMudancaCategoria(evento){
     evento.stopPropagation();
     this.categoria = evento.target.value;
@@ -39,7 +52,7 @@ class FormularioCadastro extends Component {
         >
           <option>Sem categoria</option>
 
-          {this.props.categorias.map((categoria, index) => {
+          {this.state.categorias.map((categoria, index) => {
             return <option key={index}>{categoria}</option>
           })}
         </select>
